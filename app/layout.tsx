e0dcd0ccd9bcc9547df0b191eb5876dd53649b11
Andi Irzan <serarechannel@gmail.com>
2024-10-09 18:11:30 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,12 +14,12 @@ const fontSans = FontSans({
   variable: '--font-sans'
 })
 
-const title = 'Morphic'
+const title = 'HatAI'
 const description =
-  'A fully open-source AI-powered answer engine with a generative UI.'
+  'Best Search Engine With AI-Powered'
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://morphic.sh'),
+  metadataBase: new URL('https://hat.baby'),
   title,
   description,
   openGraph: {
@@ -30,7 +30,7 @@ export const metadata: Metadata = {
     title,
     description,
     card: 'summary_large_image',
-    creator: '@miiura'
+    creator: '@zannonly_'
   }
 }
 
